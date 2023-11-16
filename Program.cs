ecb9401bd97d2395660850555df042b1a53892d8
hugeMordor <rykedfel@gmail.com>
2023-11-16 19:23:14 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace USRS3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine("Четные: {0}", arr.Count(i => i % 2 == 0));
+            Console.WriteLine("Нечетные: {0}", arr.Count(i => i % 2 == 1));
+        }
+    }
+}
